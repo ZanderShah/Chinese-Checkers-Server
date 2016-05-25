29920da9a0fcd7d2cca4137b46a1815d68932b03
CallumMoseley <callum.moseley1@gmail.com>
2016-05-25 11:41:52 -0400

    420

--- ChineseCheckersServer/src/Server.java
+++ ChineseCheckersServer/src/Server.java
@@ -66,7 +66,7 @@ public class Server extends JFrame
 		// Connect players
 		try
 		{
-			serverSocket = new ServerSocket(420);
+			serverSocket = new ServerSocket(421);
 
 			while (playersConnected != noOfPlayers)
 			{
